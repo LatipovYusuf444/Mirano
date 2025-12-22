b9fbf7d87a58d1164313cd78ae4a1b5f20f45049
Yusuf5445 <Ylatipov007@gmail.com>
2025-12-23 02:16:30 +0500

    responsiv tayyor header qismi

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,20 +2,35 @@ import { Button } from "./ui/button"
 
 const Header = () => {
   return (
-    <div className="flex flex-col mx-auto w-full px-4 pt-5 mt-20 md:mt-20">
-      <div className="rounded-3xl w-full md:w-[880px] md:h-54 sm:h-28 p-5 bg-white/10 backdrop-blur-md border border-white/10 shadow-lg">
-        <h1 className="text-3xl sm:text-4xl md:text-7xl kotta text-white leading-tight">
+    <div className="flex flex-col mx-auto w-full px-4 pt-5 mt-20 md:mt-10">
+      <div className="rounded-3xl w-72 h-22 md:w-[880px] md:h-54 sm:h-28 p-5 bg-white/10 backdrop-blur-md border border-white/10 shadow-lg">
+        <h1 className="text-xl sm:text-4xl md:text-7xl kotta text-white leading-tight">
           Jahon darajasidagi <br /> Texnik <span className="text-orange-400 kotta">to'qimachilik</span>
         </h1>
       </div>
-      <div className="w-full md:w-[800px] h-auto mt-5 bg-white/10 backdrop-blur-md border border-white/10 rounded-3xl p-5 shadow-md ">
-        <p className="text-[20px] sm:text-[16px] md:text-[20px] text-white">
+      <div className="
+  w-full 
+  md:w-[800px] 
+  h-16 
+  md:h-28 
+  mt-5 
+  bg-white/10 
+  backdrop-blur-md 
+  border 
+  border-white/10 
+  rounded-3xl 
+  p-5 
+  shadow-md
+">
+        <p className="text-[9px] sm:text-[16px] md:text-[20px] text-white">
           Marino Textile ko‘p yillardan buyon tekstil bozorida yuqori sifatli matolar va tayyor <br />
-          <span className="text-orange-400 kotta">mahsulotlar yetkazib beruvchi ishonchli hamkor hisoblanadi.</span>
+          <span className="text-orange-400 kotta">
+            mahsulotlar yetkazib beruvchi ishonchli hamkor hisoblanadi.
+          </span>
         </p>
       </div>
       <Button
-        className="border h-14 sm:h-16 w-full md:w-78 text-white kotta mt-8 md:mt-10 flex items-center justify-center gap-2
+        className="border h-14 w- sm:h-16 w-52 text-[14px] md:w-78 text-white kotta mt-8 md:mt-10 flex items-center justify-center gap-2
                    bg-white/10 backdrop-blur-md border border-white/10 shadow-lg"
         variant="button"
       >
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import bgfon from "@/assets/images/bg-textile-100.webp"
 const Home = () => {
   return (
     <div>
-      <div className="h-screen bg-cover bg-center"
+      <div className="h-screen bg-cover bg-center sm:"
         style={{ backgroundImage: `url(${bgfon})` }}>
         <Navbar />
       </div>
